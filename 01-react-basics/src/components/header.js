f7138104df7bf787072df917158b304da265c2d3
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-12-03 16:09:02 +0530

    Using Dynamic Data

--- 01-react-basics/src/components/header.js
+++ 01-react-basics/src/components/header.js
@@ -1,30 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 
 /**
  * @function getYear
  * @description Get the year of current Date
  * @param Nothing
  */
-const getYear = () => {
+/*const getYear = () => {
     const newDate = new Date();
     return newDate.getFullYear();
-}
+}*/
 
 /**
  * @constant user Object
  */
-const user = {
+/*const user = {
     name: 'Francis',
     lastName: 'Jones',
     age: 16
-};
+};*/
 
 /**
  * @function Header
- * @description Header component which return the year of current date
+ * @description function based Header component which return the year of current date
  * @param Nothing
  */
-const Header = () => {
+
+
+/*const Header = () => {
     // return <div>Date in Millisecond : {
     //     getYear()
     // }</div>
@@ -32,13 +35,35 @@ const Header = () => {
     /**
      * Shows the user Detail
      */
-    return(
-        <div>
-            <div>{user.name}</div>
-            <div>{user.lastName}</div>
-            <div>{user.age}</div>
-        </div>
-    )
-}
+/*return(
+    <div>
+        <div>{user.name}</div>
+        <div>{user.lastName}</div>
+        <div>{user.age}</div>
+    </div>
+)
 
+
+// return(
+//     <div>Header</div>
+// )
+}
+*/
+
+
+/**
+ * @function Header
+ * @description Class Based Header component which return a header
+ * @param Nothing
+ */
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div>Logo</div>
+                <input type="text" />
+            </header>
+        )
+    }
+}
 export default Header;
