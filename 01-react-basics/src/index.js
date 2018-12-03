874bc31357fcb71befd0a74bb7d9d7e910d19a82
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-12-03 14:37:52 +0530

    React JSX

--- 01-react-basics/src/index.js
+++ 01-react-basics/src/index.js
@@ -2,7 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <h1>Hello World!!!</h1>
+    /**
+     * Creating element inside the root element
+     */
+
+    // return React.createElement('h1',{className: 'title'}, React.createElement('div'));
+
+    /**
+     * Multiple thing to return 
+     */
+
+    return (
+        /**
+         * We can return only thing. So we nned warp all return into single element
+         */
+        <div className="test">
+            <h1>Hello World</h1>
+            <div>Hey!!!!</div>
+        </div>
+    )
+
+    /**
+     * For return singal arguments
+     */
+
+    //   return <h1>Hello World !!!!!</h1>
 }
 
-ReactDOM.render(<App/>,document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
