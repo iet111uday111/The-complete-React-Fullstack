060022e08ee9b54224f19e301ff8fc07a242daa6
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-12-04 16:01:01 +0530

    Props cont.

--- 01-react-basics/src/components/news-list.js
+++ 01-react-basics/src/components/news-list.js
@@ -7,9 +7,20 @@ import React from 'react';
  */
 const NewsList = (props) => {
     console.log(props);
-    
+    const items = props.news.map((item) => {
+        return(
+            <div>
+                <h3>{item.title}</h3>
+                <div>{item.feed}</div>
+                <div>{item.feed}</div>
+            </div>
+        )
+    });
+
     return(
-        <div>{props.name}</div>
+        <div>
+            {items}
+        </div>
     )
 }
 
