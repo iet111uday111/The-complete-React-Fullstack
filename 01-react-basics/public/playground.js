36b960175c132ad09cf43020cbed4c4c9687dbaf
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-12-04 14:05:39 +0530

    More on classes

---
+++ 01-react-basics/public/playground.js
@@ -0,0 +1,18 @@
+class Car{
+    constructor(){
+        this.wheels = 4;
+        this.type = 'Hatchback';
+    }
+}
+
+class Ford extends Car{
+    constructor(){
+        super();
+        this.windows = 6;
+    }
+}
+
+const car = new Car();
+const ford = new Ford();
+
+console.log(ford.windows);
