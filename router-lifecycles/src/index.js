a2cd824189a8e13f08f7d0e13356fc57a4a4c5ad
Unknown <geekworks2017@gmail.com>
2018-12-05 12:12:38 +0530

    Creating Routes

--- router-lifecycles/src/index.js
+++ router-lifecycles/src/index.js
@@ -1,1 +1,34 @@
-console.log(test2);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+/**
+ * Importing Components 
+ */
+import Home from './components/home';
+import Post from './components/post';
+import Profile from './components/profile';
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <header>Header</header>
+          <Route path="/" exact component={Home} />
+          <Route path="/posts" component={Post} />
+          <Route path="/profile" component={Profile} />
+          
+        </div>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
+
+export default App;
+
