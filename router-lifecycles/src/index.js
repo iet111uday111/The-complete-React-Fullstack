cc2541ba4f04788513ca82e2fe622bd9b21dab59
Unknown <geekworks2017@gmail.com>
2018-12-05 12:55:13 +0530

    Link in Router

--- router-lifecycles/src/index.js
+++ router-lifecycles/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 /**
  * Importing Components 
  */
 import Home from './components/home';
-import Post from './components/post';
+import Posts from './components/posts';
 import Profile from './components/profile';
 
 const App = () => {
@@ -14,9 +14,18 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
-          <header>Header</header>
+          <header>
+            <Link to="/">Home</Link><br/>
+            <Link to="/posts">Posts</Link><br/>
+            <Link to={{
+              pathname:'/profile',
+              // hash: '#fransis',
+              // search:'?profile=true'
+            }}>Profile</Link><br/>
+            <hr/>
+          </header> 
           <Route path="/" exact component={Home} />
-          <Route path="/posts" component={Post} />
+          <Route path="/posts" component={Posts} />
           <Route path="/profile" component={Profile} />
           
         </div>
