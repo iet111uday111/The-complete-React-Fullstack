42e82e94e0fbad603d615face373760f3c0c73e9
Unknown <geekworks2017@gmail.com>
2018-12-05 15:35:48 +0530

    Router Switch

--- router-lifecycles/src/index.js
+++ router-lifecycles/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Link, NavLink, Switch } from 'react-router-dom';
 // import { HashRouter, Route, Link } from 'react-router-dom';
 // import { MemoryRouter, Route, Link } from 'react-router-dom';
 
@@ -18,24 +18,31 @@ const App = () => {
       <BrowserRouter>
         <div>
           <header>
-            <NavLink to="/">Home</NavLink><br/>
-            <NavLink 
-                to="/posts"
-                activeStyle={{color: 'red'}}
-                activeClassName="selected"
-            >Posts</NavLink><br/>
+            <NavLink to="/">Home</NavLink><br />
+            <NavLink
+              to="/posts"
+              activeStyle={{ color: 'red' }}
+              activeClassName="selected"
+            >Posts</NavLink><br />
             <NavLink to={{
-              pathname:'/profile',
+              pathname: '/profile',
               // hash: '#fransis',
               // search:'?profile=true'
-            }}>Profile</NavLink><br/>
-            <hr/>
-          </header> 
-          <Route path="/" exact component={Home} />
+            }}>Profile</NavLink><br />
+            <hr />
+          </header>
+          {/* <Route path="/" exact component={Home} />
           <Route path="/posts" exact component={Posts} />
           <Route path="/posts/:id/:username" component={PostItem} />
-          <Route path="/profile" component={Profile} />
-          
+          <Route path="/profile" component={Profile} /> */}
+
+          <Switch>
+            <Route path="/posts/:id/:username" component={PostItem} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/" component={Home} />
+          </Switch>
+
         </div>
       </BrowserRouter>
     </div>
