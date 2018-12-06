26838312700dfc5b97ceb26bfb09c5c7e9f5800f
Unknown <geekworks2017@gmail.com>
2018-12-06 12:05:33 +0530

    Using react-transition-group

--- transitions-template/src/components/Transition.js
+++ transitions-template/src/components/Transition.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition';
+
 import '../css/App.css';
 
-class TransitionComp extends Component{
+class TransitionComp extends Component {
 
-    render(){
-        return(
+    state = {
+        show: true
+    }
+
+    showDiv = () => {
+        this.setState({
+            show: !this.state.show ? true : false
+        });
+    }
+
+    render() {
+        return (
             <div>
-                Transition
+                <Transition in={this.state.show} timeout={2000} unmountOnExit>
+                    {state => 
+                        <div style={{
+                            background: 'red',
+                            height: '100px',
+                            transition: 'all 2s ease',
+                            opacity: state === 'exited' || state === 'exiting' ? 0 : 1
+                        }}>
+                            {state}
+                        </div>
+                    }
+                </Transition>
+
+                {/* { this.state.show ? <div style={{
+                    background: 'red',
+                    height: '100px'
+                }}>
+                </div> : null }
+                 */}
+
+                <div className="showDiv" onClick={this.showDiv}>
+                    Show or Hide
+                </div>
             </div>
 
         )
