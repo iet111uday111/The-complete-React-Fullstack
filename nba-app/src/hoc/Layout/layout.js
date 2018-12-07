5885700b40964d0f346d49b9aabc2b8d373892fb
Unknown <geekworks2017@gmail.com>
2018-12-07 13:09:36 +0530

    SideNav setup

--- nba-app/src/hoc/Layout/layout.js
+++ nba-app/src/hoc/Layout/layout.js
@@ -7,13 +7,23 @@ import Header from '../../components/Header/header';
 export default class Layout extends Component {
 
     state = {
+        showNav: false
+    }
 
+    toggleSidenav = (action) => {
+        this.setState({
+            showNav: action
+        })
     }
 
     render() {
         return (
             <div>
-            <Header />
+            <Header 
+                showNav = { this.state.showNav }
+                onHideNav = { ()=> this.toggleSidenav(false)}
+                onOpenNav = { () => this.toggleSidenav(true)}
+            />
             {this.props.children}
             Footer
             </div>
