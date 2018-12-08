d509cbb1b9784c865de948acd8072781702e0630
Unknown <geekworks2017@gmail.com>
2018-12-08 19:28:57 +0530

    :punch: News List

--- nba-app/src/components/Home/home.js
+++ nba-app/src/components/Home/home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import NewsSlider from '../widgets/NewsSlider/slider';
+import NewsList from '../widgets/NewsList/newsList';
 
 const Home = () => {
   return (
     <div>
-        <NewsSlider type="featured" start={0} amount={6} settings={{ dots: false }}/>
+        <NewsSlider type="featured" start={0} amount={3} settings={{ dots: false }}/>
+        <NewsList type="card" loadmore={true} start={3} amount={3} />
     </div>
   )
 }
--- nba-app/src/components/widgets/NewsSlider/slider.js
+++ nba-app/src/components/widgets/NewsSlider/slider.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import SliderTemplate from './sliderTemplate';
+import { URL } from '../../../config';
 
 
 export default class NewsSlider extends Component{
@@ -9,7 +10,7 @@ export default class NewsSlider extends Component{
     }
 
     componentWillMount(){
-        axios.get(`http://localhost:3004/articles?_start=${this.props.start}&_end=${this.props.amount}`)
+        axios.get(`${URL}/articles?_start=${this.props.start}&_end=${this.props.amount}`)
             .then((response) =>{
                 console.log(response.data);
                 this.setState({
