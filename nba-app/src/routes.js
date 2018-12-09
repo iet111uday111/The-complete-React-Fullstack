749b874cfb50462db9ebc909d658c8456934f0b6
Unknown <geekworks2017@gmail.com>
2018-12-09 14:48:10 +0530

    Article Post View

--- nba-app/src/routes.js
+++ nba-app/src/routes.js
@@ -7,6 +7,7 @@ import { Route, Switch } from 'react-router-dom';
  */
 import Home from './components/Home/home';
 import Layout from './hoc/Layout/layout';
+import NewsArticle from './components/Articles/News/Post';
 
 
 export default class Routes extends Component {
@@ -15,6 +16,7 @@ export default class Routes extends Component {
             <Layout>
                 <Switch>
                     <Route path="/" exact component={Home} />
+                    <Route path="/articles/:id" exact component={NewsArticle} />
                 </Switch>
             </Layout>
         )
