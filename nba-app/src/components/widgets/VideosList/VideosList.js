24263c9f18ea32b8a2e9fcc94fb25c1391dbb4a4
Unknown <geekworks2017@gmail.com>
2018-12-09 13:39:49 +0530

    Video List

---
+++ nba-app/src/components/widgets/VideosList/VideosList.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import styles from  './videosList.css';
+import axios from 'axios';
+
+import { URL } from '../../../config';
+import Button from '../Buttons/buttons';
+
+export default class VideosList extends Component {
+    state = {
+        teams:[],
+        videos:[],
+        start: this.props.start,
+        end: this.props.start + this.props.amount,
+        amount: this.props.amount
+    }
+
+   renderTitle = () => {
+       return this.props.title ? 
+            <h3><strong>NBA</strong>Videos</h3> 
+       : null
+   }
+
+   loadMore = () => {
+       
+   }
+
+   renderButton = () => {
+        return this.props.loadmore ?
+        <Button type="loadmore" loadMore={()=> this.loadMore()} cta="Load More Videos"/>
+         : 
+          <Button type="linkTo" cta="More Videos" linkTo="/videos" />
+   }
+  render() {
+    return (
+      <div className={styles.videoList_wrapper}>
+            {this.renderTitle()}
+            { this.renderButton() }
+      </div>
+    )
+  }
+}
