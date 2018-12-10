74211fc8f90c556df87d69a0ee15ad5b1be634bd
Unknown <geekworks2017@gmail.com>
2018-12-10 14:18:26 +0530

    :punch: Form Templating

--- forms/src/components/user.js
+++ forms/src/components/user.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
+import FormFields from './widgets/Forms/formFields';
 
 class User extends Component {
 
     state = {
-        
+        formData: {
+            name: {
+                element: 'input',
+                value: '',
+                label:true,
+                labelText: 'Name',
+                config: {
+                    name: 'name_input',
+                    type: 'text',
+                    placeholder: 'Enter your name',
+
+                }
+            },
+            lastname: {
+                element: 'input',
+                value: '',
+                label:true,
+                labelText: 'Lastname',
+                config: {
+                    name: 'lastname_input',
+                    type: 'text',
+                    placeholder: 'Enter your Lastname',
+                    
+                }
+            }
+        }
     }
 
     render(){
         return(
-            <div>
-                User
+            <div className="container">
+                <form onSubmit={this.submitForm}>
+                    <FormFields 
+                        formData={this.state.formData}
+                            
+                    />
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         )
     }
