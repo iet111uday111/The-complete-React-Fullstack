bc1a30c3d8d6e0585f2a17286b29a79a452982fd
Unknown <geekworks2017@gmail.com>
2018-12-10 17:49:11 +0530

    :punch: Firebase Startup

---
+++ forms/src/firebase.js
@@ -0,0 +1,23 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyAwfM1nJbiU7a7WA_unNrANBBEh0EhjAHs",
+    authDomain: "react-forms-e19c1.firebaseapp.com",
+    databaseURL: "https://react-forms-e19c1.firebaseio.com",
+    projectId: "react-forms-e19c1",
+    storageBucket: "react-forms-e19c1.appspot.com",
+    messagingSenderId: "808650809020"
+};
+
+firebase.initializeApp(config);
+
+// const firebaseDB = firebase.database();
+// const googleAuth = new firebase.auth.GoogleAuthProvider();
+
+// export {
+//     firebase,
+//     firebaseDB,
+//     googleAuth
+// }
+
+firebase.database().ref().set('It Works');
