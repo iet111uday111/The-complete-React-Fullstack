5eb315f0c6e72417253895258348d22d67f526b0
Unknown <geekworks2017@gmail.com>
2018-12-11 17:26:10 +0530

    :punch: Signin Form and Form Field 3

--- nba-app/src/components/SignIn/SignIn.js
+++ nba-app/src/components/SignIn/SignIn.js
@@ -4,99 +4,190 @@ import { firebase } from '../../firebase';
 
 import FormField from '../widgets/FormsFields/formFields';
 
-export default class SignIn extends Component {
+class SignIn extends Component {
+
     state = {
-        registerError: '',
-        loading: false,
-        formdata: {
-            email: {
-                element: 'input',
-                value: '',
-                config: {
-                    name: 'email_input',
-                    type: 'email',
-                    placeholder: 'Enter your email'
+        registerError:'',
+        loading:false,
+        formdata:{
+            email:{
+                element:'input',
+                value:'',
+                config:{
+                    name:'email_input',
+                    type:'email',
+                    placeholder:'Enter your email'
                 },
-                validation: {
-                    required: true,
-                    email: true
+                validation:{
+                    required:true,
+                    email:true
                 },
-                valid: false,
-                touched: false,
-                validationMessage: ''
+                valid:false,
+                touched:false,
+                validationMessage:''
             },
-            password: {
-                element: 'input',
-                value: '',
-                config: {
-                    name: 'password_input',
-                    type: 'password',
-                    placeholder: 'Enter your password'
+            password:{
+                element:'input',
+                value:'',
+                config:{
+                    name:'password_input',
+                    type:'password',
+                    placeholder:'Enter your password'
                 },
-                validation: {
-                    required: true,
-                    password: true
+                validation:{
+                    required:true,
+                    password:true
                 },
-                valid: false,
-                touched: false,
-                validationMessage: ''
+                valid:false,
+                touched:false,
+                validationMessage:''
             }
         }
     }
 
     updateForm = (element) => {
-        const newFormData = {
+        const newFormdata = {
             ...this.state.formdata
         }
-
         const newElement = {
-            ...newFormData[element.id]
+            ...newFormdata[element.id]
         }
-
-        
         newElement.value = element.event.target.value;
-
-        newFormData[element.id] = newElement;
-        
+        if(element.blur){
+            let validData = this.validate(newElement);
+            newElement.valid = validData[0];
+            newElement.validationMessage = validData[1];   
+        }
+        newElement.touched = element.blur;
+        newFormdata[element.id] = newElement;
         
         this.setState({
-            formdata: newFormData
-        });
-
-
-
-        
+            formdata:newFormdata
+        })
     }
 
-    showButton = () => {
-        console.log('showButton');
+    validate = (element) => {
+        let error = [true,''];
+
+        if(element.validation.email){
+            const valid = /\S+@\S+\.\S+/.test(element.value);
+            const message = `${!valid ? 'Must be a valid email':''}`;
+            error = !valid ? [valid,message] : error
+        }
+
+        if(element.validation.password){
+            const valid = element.value.length >= 5;
+            const message = `${!valid ? 'Must be greater than 5':''}`;
+            error = !valid ? [valid,message] : error
+        }
+
+        if(element.validation.required){
+            const valid = element.value.trim() !=='';
+            const message = `${!valid ? 'This field is required':''}`;
+            error = !valid ? [valid,message] : error
+        }
+
+        return error;
     }
 
-    showError = () => {
-        console.log('showError');
+    submitForm = (event,type) => {
+        event.preventDefault();
+
+        if(type !== null){
+
+            let dataToSubmit = {};
+            let formIsValid = true;
+
+            for(let key in this.state.formdata){
+                dataToSubmit[key] = this.state.formdata[key].value
+            }
+            for(let key in this.state.formdata){
+                formIsValid = this.state.formdata[key].valid && formIsValid;
+            }
+
+            if(formIsValid){
+                this.setState({
+                    loading:true,
+                    registerError:''
+                })
+                if(type){
+                    firebase.auth()
+                    .signInWithEmailAndPassword(
+                        dataToSubmit.email,
+                        dataToSubmit.password
+                    ).then(()=>{
+                        this.props.history.push('/')
+                    }).catch( error =>{
+                        this.setState({
+                            loading:false,
+                            registerError: error.message
+                        })
+                    })
+
+                } else {
+                    firebase.auth()
+                    .createUserWithEmailAndPassword(
+                        dataToSubmit.email,
+                        dataToSubmit.password
+                    ).then(()=>{
+                        this.props.history.push('/')
+                    }).catch( error =>{
+                        this.setState({
+                            loading:false,
+                            registerError: error.message
+                        })
+                    })
+                }
+            }
+        }
+
+
     }
 
-    render() {
-        return (
+
+    submitButton = () => (
+        this.state.loading ? 
+            'loading...'
+        :
+        <div>
+            <button onClick={(event)=>this.submitForm(event,false)}> Register now</button>
+            <button onClick={(event)=>this.submitForm(event,true)}> Log in </button>
+        </div>
+    )
+
+    showError = () => (
+        this.state.registerError !== '' ? 
+            <div className={styles.error}>{this.state.registerError}</div>
+        : ''
+    )
+
+    render(){
+        return(
             <div className={styles.logContainer}>
-                <form>
+                <form onSubmit={(event)=>this.submitForm(event,null)}>
+                    <h2>Register / Log in</h2>
+
                     <FormField
                         id={'email'}
-                        formData={this.state.formdata.email}
-                        change={(element) => this.updateForm(element)}   
+                        formdata={this.state.formdata.email}
+                        change={(element)=>this.updateForm(element)}
                     />
+
+                    
 
                     <FormField
                         id={'password'}
-                        formData={this.state.formdata.password}
-                        change={(element) => this.updateForm(element)}   
+                        formdata={this.state.formdata.password}
+                        change={(element)=>this.updateForm(element)}
                     />
 
-                    {this.showButton()}
-                    {this.showError()}
-
+                    { this.submitButton() }
+                    { this.showError() }
                 </form>
             </div>
         )
     }
+
 }
+
+export default SignIn;
--- nba-app/src/components/widgets/FormsFields/formFields.js
+++ nba-app/src/components/widgets/FormsFields/formFields.js
@@ -1,37 +1,70 @@
-import React from 'react'
+import React from 'react';
+import styles from './formFields.css';
 
-const FormFields = ({formData, change, id}) => {
+
+const FormFields = ({formdata,change,id}) =>{
+
+    const showError = () => {
+        let errorMessage = null;
+
+        if(formdata.validation && !formdata.valid){
+            errorMessage = (
+                <div className={styles.labelError}>
+                    {formdata.validationMessage}
+                </div>
+            )
+        }
+
+        return errorMessage;
+    }
+
 
     const renderTemplate = () => {
         let formTemplate = null;
-        
-        switch (formData.element) {
-            case'input':
+
+        switch(formdata.element){
+            case('input'):
                 formTemplate = (
                     <div>
-                        <input 
-                            {...formData.config}
-                            value={formData.value}
+                        <input
+                            {...formdata.config}
+                            value={formdata.value}
                             onBlur={(event) => change({event,id,blur:true})}
                             onChange={(event) => change({event,id,blur:false})}
                         />
+                        { showError() }
                     </div>
                 )
                 break;
-        
+            case('select'):
+                formTemplate = (
+                    <div>
+                        <select
+                            value={formdata.value}
+                            name={formdata.config.name}
+                            onBlur={(event) => change({event,id,blur:true})}
+                            onChange={(event) => change({event,id,blur:false})}
+                        >
+                            { formdata.config.options.map((item,i)=>(
+                                <option key={i} value={item.id}>{item.name}</option>
+                            ))}
+
+                        </select>
+                    </div>
+                )
+                break;
             default:
                 formTemplate = null;
-                break;
         }
-
         return formTemplate;
     }
 
-    return (
+    return(
         <div>
             {renderTemplate()}
         </div>
     )
+
 }
 
-export default FormFields
+export default FormFields;
