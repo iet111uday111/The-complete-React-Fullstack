e80a199eed1eb0874565fd87d6f6f37c2ffb83ab
Unknown <geekworks2017@gmail.com>
2018-12-13 13:51:23 +0530

    :punch:Created Movies Reducer

--- redux-basic/src/index.js
+++ redux-basic/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
 
 const App = () => (
     <div>Hello</div>
@@ -10,7 +11,7 @@ const App = () => (
 const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(
-    <Provider store={createStoreWithMiddleware()}>
+    <Provider store={createStoreWithMiddleware(reducers)}>
         <App />
     </Provider>
     
