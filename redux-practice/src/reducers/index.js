8036095d9d2e4061a9aa255129204ee52dc12cf0
Unknown <geekworks2017@gmail.com>
2018-12-13 19:09:41 +0530

    :punch: Final Commit

--- redux-practice/src/reducers/index.js
+++ redux-practice/src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import artists from './artist-reducers';
+import artists from './artist_reducer';
 
 const rootReducer = combineReducers({
     artists
-});
+})
 
 export default rootReducer;
