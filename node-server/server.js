7b6e59fcf9dc794f2eab0d478220e780d42e9c21
Unknown <geekworks2017@gmail.com>
2018-12-14 14:00:12 +0530

    :punch: Creating Routes without Any Framework

--- node-server/server.js
+++ node-server/server.js
@@ -12,24 +12,32 @@ let jsonData = {
 /**
  * Sending Plain Text
  */
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type': 'text/plain'});
-    res.end('whatever');  
-});
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-type': 'text/plain'});
+//     res.end('whatever');  
+// });
 /**
  * Sending HTML
  */
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type': 'text/html'});
-    res.end(html);  
-});
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-type': 'text/html'});
+//     res.end(html);  
+// });
 
 /**
  * Sending JSON
  */
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type': 'application/json'});
-    res.end(JSON.stringify(jsonData));  
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.end(html);
+    } else if (req.url === '/api/user') {
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(JSON.stringify(jsonData));
+    }else{
+        res.writeHead(404, { 'Content-type': 'text/html' });
+        res.end('Not Found')
+    }
 });
 
 server.listen(PORT, hostname, () => {
