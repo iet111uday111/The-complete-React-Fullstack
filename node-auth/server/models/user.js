2979e03a70c7e9bd2a51514bb1b714554918f031
uday <geekworks2017@gmail.com>
2018-12-17 13:50:01 +0530

    hashing password and compare password

--- node-auth/server/models/user.js
+++ node-auth/server/models/user.js
@@ -1,4 +1,6 @@
 const moongose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_I = 10;
 
 const userSchema = moongose.Schema({
     email: {
@@ -14,6 +16,28 @@ const userSchema = moongose.Schema({
     }
 });
 
+userSchema.pre('save', function(next){
+    var user = this;
+    if(user.isModified('password')){
+        bcrypt.genSalt(SALT_I, (err, salt) => {
+            if(err) return next(err);
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            })  
+        })
+    }else{
+        next();
+    }
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb){
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+        if(err) throw cb(err);
+        cb(null, isMatch);
+    });
+}
 
 const User = moongose.model('User', userSchema);
 
--- node-auth/server/server.js
+++ node-auth/server/server.js
@@ -3,12 +3,11 @@ const bodyParser = require('body-parser');
 const moongose = require('mongoose');
 
 const app = express();
-
 moongose.Promise = global.Promise;
-
 moongose.connect('mongodb://localhost:27017/node-auth');
 
 const { User } = require('./models/user');
+
 app.use(bodyParser.json());
 
 
@@ -25,6 +24,20 @@ app.post('/api/user', (req, res) =>{
         res.status(200).send(doc);
 
     })
+});
+
+app.post('/api/user/login', (req, res) => {
+    User.findOne({email: req.body.email},(err, user) => {
+        if(!user) res.json({message: 'Auth failed, user not found'});
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(err) throw err;
+            if(!isMatch) return res.status(400).json({
+                message: 'Wrong Password'
+            }) 
+            res.status(200).send(isMatch); 
+        })
+        
+    });
 })
 
 
