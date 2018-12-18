e39e674377deb2b1d9ae504ab89b5c0155fc89c6
uday <geekworks2017@gmail.com>
2018-12-18 11:59:54 +0530

    server book routes

--- book-shelf/server/server.js
+++ book-shelf/server/server.js
@@ -14,6 +14,54 @@ const { Book } = require('./models/book');
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+/**
+ * GET Requests
+ */
+app.get('/api/getBook', (req, res) => {
+    let id = req.query.id;
+
+    Book.findById(id, (err, doc) => {
+        if(err) return res.status(400).send(err);
+        res.send(doc);
+    })
+});
+
+
+app.get('/api/books', (req, res) => {
+    let skip = parseInt(req.query.skip);
+    let limit = parseInt(req.query.limit); 
+    let order = req.query.order; 
+
+    Book.find().skip(skip).sort({_id:order}).limit(limit).exec((err, doc) => {
+        if(err) return res.status(400).send(err);
+        res.send(doc);
+    })
+});
+
+/**
+ * POST Requests
+ */
+app.post('/api/book',(req,res) => {
+
+    const book = new Book(req.body);
+    book.save((err, doc) => {
+        if(err) return res.status(400).send(err);
+        res.status(200).json({
+            post: true,
+            book: doc._id
+        });
+    });
+});
+
+/**
+ * PUT Requests
+ */
+
+
+/**
+ * DELETE Requests
+ */
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Server rinning on ${port} port.`);
