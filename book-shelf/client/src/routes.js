4e76a90fea84581882fa9422fc35ab7c04e9da99
Unknown <geekworks2017@gmail.com>
2018-12-19 19:02:54 +0530

    Created Add Review Form

--- book-shelf/client/src/routes.js
+++ book-shelf/client/src/routes.js
@@ -6,6 +6,7 @@ import BookView from './components/Books';
 import Login from './containers/Admin/login';
 import Auth from './hoc/auth';
 import User from './components/Admin';
+import AddReview from './containers/Admin/add';
 
 const Routes = () => {
   return (
@@ -14,6 +15,7 @@ const Routes = () => {
         <Route path="/" exact component={Auth(Home, null)} />
         <Route path="/login" exact component={Auth(Login, false)} />
         <Route path="/user" exact component={Auth(User, true)} />
+        <Route path="/user/add" exact component={Auth(AddReview,true)}/>
         <Route path="/books/:id" exact component={Auth(BookView)} />
       </Switch>
     </Layout>
