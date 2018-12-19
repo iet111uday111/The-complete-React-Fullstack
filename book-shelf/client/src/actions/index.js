78caa6b664eec42a765e7b978d1c0ad409bc0e02
Unknown <geekworks2017@gmail.com>
2018-12-19 15:05:29 +0530

    Make an action  getBookWithReviewer using redux-thunk and getting data on client side

--- book-shelf/client/src/actions/index.js
+++ book-shelf/client/src/actions/index.js
@@ -24,6 +24,14 @@ export function getBooks(
 
 }
 
+/**
+ * Redux Thunk middleware allows you to write action creators that 
+ * return a function instead of an action. The thunk can be used to 
+ * delay the dispatch of an action, or to dispatch only if a certain 
+ * condition is met. The inner function receives the store methods 
+ * dispatch and getState as parameters.
+ * @param id 
+ */
 export function getBookWithReviewer(id){
     const request = axios.get(`/api/getBook?id=${id}`)
 
