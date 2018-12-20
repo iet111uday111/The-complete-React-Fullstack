e10b4c89b92c49be0cdbbe7f2ca569e9c4d8bacb
uday <geekworks2017@gmail.com>
2018-12-20 17:39:35 +0530

    Cleanup

--- book-shelf/client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import books from './books_reducer';
-import user from './user_reducer';
-const rootReducers = combineReducers({
-    books,
-    user
-});
-
-export default rootReducers;
