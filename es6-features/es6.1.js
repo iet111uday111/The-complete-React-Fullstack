0f3c817ae5a0218ce8f634677bd6d15cb12dec89
Unknown <geekworks2017@gmail.com>
2018-12-21 18:09:47 +0530

    Real life example

--- es6-features/es6.1.js
+++ es6-features/es6.1.js
@@ -16,12 +16,54 @@
  * ES6
  */
 
-const value = 10;
-let winner = false;
+// const value = 10;
+// let winner = false;
 
-if(value == 10){
-    let winner = true;
-    console.log(`Inside if: ${winner}`);
+// if(value == 10){
+//     let winner = true;
+//     console.log(`Inside if: ${winner}`);
+// }
+
+// console.log(`outside if: ${winner}`);
+
+
+/**
+ * ES6
+ */
+
+/**
+ * Every Time it Shows : You have clicked Div No. 4
+ */
+// document.onreadystatechange = function () {
+//     if (document.readyState === 'complete') {
+//         var Divs = document.getElementsByTagName("div");
+//         console.log(Divs);
+
+//         for (var index = 0; index < Divs.length; index++) {
+//             Divs[index].addEventListener('click', function () {
+//                 console.log('You have clicked Div No.', index);
+//             })
+//         }
+//     }
+// }
+
+
+/**
+ * You have clicked Div No. 0
+ *You have clicked Div No. 1
+ *You have clicked Div No. 2
+ *You have clicked Div No. 3
+ * let created it's own scope.
+ */
+document.onreadystatechange = function () {
+    if (document.readyState === 'complete') {
+        var Divs = document.getElementsByTagName("div");
+        console.log(Divs);
+
+        for (let index = 0; index < Divs.length; index++) {
+            Divs[index].addEventListener('click', function () {
+                console.log('You have clicked Div No.', index);
+            })
+        }
+    }
 }
-
-console.log(`outside if: ${winner}`);
