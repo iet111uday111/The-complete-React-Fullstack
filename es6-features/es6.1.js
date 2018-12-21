50438d8eb800516dd9be1aed74cbd29f33ccd4ca
Unknown <geekworks2017@gmail.com>
2018-12-21 17:47:03 +0530

    let & const application

---
+++ es6-features/es6.1.js
@@ -0,0 +1,27 @@
+/**
+ * Javascript
+ */
+// var winner = true;
+
+// function user(){
+//     var winners = false;
+// }
+
+// user();
+
+// console.log(winner);
+// console.log(winners);
+
+/**
+ * ES6
+ */
+
+const value = 10;
+let winner = false;
+
+if(value == 10){
+    let winner = true;
+    console.log(`Inside if: ${winner}`);
+}
+
+console.log(`outside if: ${winner}`);
