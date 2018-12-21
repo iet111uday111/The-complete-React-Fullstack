dfb9ace323b9897513166c4e5c53f636383398bd
Unknown <geekworks2017@gmail.com>
2018-12-21 14:28:59 +0530

    Template Setup for Redux-type

---
+++ redux-types/src/reducers/data_reducer.js
@@ -0,0 +1,12 @@
+export default function(state={}, action){
+    switch(action.type){
+        case 'GET_MOVIES':
+            return {...state,movies:action.payload};
+        case 'GET_GAMES':
+            return {...state,games:action.payload};
+        case 'GET_CELEBRITIES':
+            return {...state,celebrities:action.payload};
+        default:
+            return state;
+    }
+}
