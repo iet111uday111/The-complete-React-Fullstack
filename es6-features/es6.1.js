148230abfb5279d219fbe6cdf5b52a81eb1b4a72
Unknown <geekworks2017@gmail.com>
2018-12-21 18:24:13 +0530

    Excercises

--- es6-features/es6.1.js
+++ es6-features/es6.1.js
@@ -1,30 +1,30 @@
 /**
  * Javascript
  */
-// var winner = true;
+var winner = true;
 
-// function user(){
-//     var winners = false;
-// }
+function user(){
+    var winners = false;
+}
 
-// user();
+user();
 
-// console.log(winner);
-// console.log(winners);
+console.log(winner);
+console.log(winners);
 
 /**
  * ES6
  */
 
-// const value = 10;
-// let winner = false;
+const value = 10;
+let winner = false;
 
-// if(value == 10){
-//     let winner = true;
-//     console.log(`Inside if: ${winner}`);
-// }
+if(value == 10){
+    let winner = true;
+    console.log(`Inside if: ${winner}`);
+}
 
-// console.log(`outside if: ${winner}`);
+console.log(`outside if: ${winner}`);
 
 
 /**
@@ -34,18 +34,18 @@
 /**
  * Every Time it Shows : You have clicked Div No. 4
  */
-// document.onreadystatechange = function () {
-//     if (document.readyState === 'complete') {
-//         var Divs = document.getElementsByTagName("div");
-//         console.log(Divs);
+document.onreadystatechange = function () {
+    if (document.readyState === 'complete') {
+        var Divs = document.getElementsByTagName("div");
+        console.log(Divs);
 
-//         for (var index = 0; index < Divs.length; index++) {
-//             Divs[index].addEventListener('click', function () {
-//                 console.log('You have clicked Div No.', index);
-//             })
-//         }
-//     }
-// }
+        for (var index = 0; index < Divs.length; index++) {
+            Divs[index].addEventListener('click', function () {
+                console.log('You have clicked Div No.', index);
+            })
+        }
+    }
+}
 
 
 /**
@@ -67,3 +67,69 @@ document.onreadystatechange = function () {
         }
     }
 }
+
+/**
+ * Excercise 1
+ * Which of these variables would you change to let or const ?
+ * 
+ */
+
+var name = "James";
+var employeeId = 25;
+var eyes = "Blue";
+var age = 25
+var position = "Developer";
+var salary = 2500;
+var knowdlege = ['Javascript','PHP','REACT','Angular 4'];
+var married = false;
+
+/**
+ * Solution
+ */
+
+const name = "James";
+const employeeId = 25;
+const eyes = "Blue";
+let age = 25
+let position = "Developer";
+let salary = 2500;
+let knowdlege = ['Javascript','PHP','   ','Angular 4'];
+let married = false;
+
+/**
+ * Excercise
+ */
+
+const cupcake = document.querySelector('.cupcake');
+const container = document.querySelector('.orders')
+
+const cupcakes = [
+ {id:1,flavor:'Strawberry'},
+ {id:2,flavor:'Apple'},
+ {id:3,flavor:'Banana'}
+]
+
+let randomCupcake;
+let orderNumber = 0;
+const baseAmount = 5;
+let finalAmount;
+const dicountID = 2;
+const dicountAmount = 3;
+let message
+
+cupcake.addEventListener("click",function(){
+ // Select a random cupcake
+ randomCupcake = Math.floor(Math.random() * cupcakes.length) + 0;
+
+ // Increment the order number, we get a different order with each cupcake.
+ orderNumber += 1;
+
+ // Depending on the "dicountID" variable, this cupcake is on sale or not.
+ finalAmount = dicountID == cupcakes[randomCupcake].id ? (baseAmount -dicountAmount ):baseAmount;
+
+ // Create the message
+ message = '<div>- You won a ' + cupcakes[randomCupcake].flavor + ' for ' + finalAmount + '$ , your order # is '+ orderNumber + '</div>'
+ container.insertAdjacentHTML('beforeend',message)
+
+});
+
