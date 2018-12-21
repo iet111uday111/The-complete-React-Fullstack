2a0cb82a9b77678f40e4be5aba871f304529520e
uday <geekworks2017@gmail.com>
2018-12-21 14:24:12 +0530

    cleaning

--- webpack/project/index.js
+++
@@ -1,5 +0,0 @@
-import data from './users';
-import './css/style.css';
-import './css/user.css';
-
-console.log(`My name is ${data.name}, i am ${data.age} years old`);
--- webpack/project/users.js
+++
@@ -1,6 +0,0 @@
-const data = {
-    age:25,
-    name:"Francis"
-}
-
-export default data;
