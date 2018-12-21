ad956c32640fd48b856b5120f2374a158d5362e5
Unknown <geekworks2017@gmail.com>
2018-12-21 12:45:54 +0530

    Webpack with react setup

---
+++ webpack-react/webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        bundle: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/,
+                exclude:/node_modules/
+            }
+
+        ]
+    }
+
+}
